796c2fafc99c73ee0cefb4a1327ce9b0a826223e
Hyeonseok Oh <hyeonso5@gmail.com>
2016-10-16 07:07:05 +0000

    fixed fatal error in learn_with_linear_svc

--- category_learn_linear_svc.py
+++ category_learn_linear_svc.py
@@ -7,15 +7,16 @@ from sklearn.svm import LinearSVC
 from sklearn.externals import joblib
 
 def learn_with_linear_svc(x_list, s_list):
-    svc_param = {'alpha':np.logspace(-2, 0, 20)}
+    svc_param = {'C':np.logspace(-2, 0, 20)}
     gs_svc = GridSearchCV(LinearSVC(),svc_param,cv=5,n_jobs=4)
     gs_svc.fit(x_list, s_list)
     print gs_svc.best_params_
     print 'score : ' + str(gs_svc.best_score_)
     clf = LinearSVC(C=gs_svc.best_params_['C'])
-    clf = MultinomialNB(alpha=gs_svc.best_params_['alpha'])
     clf.fit(x_list, s_list)
+    print 'model initialized. dump running'
     joblib.dump(clf,'jisik_classify_linear_svc.model',compress=3)
+    print 'done'
 
 if __name__ == "__main__":
     np.random.seed(0)
@@ -24,8 +25,8 @@ if __name__ == "__main__":
     print "load complete"
     cnt = x_list.shape[0]
     indices = np.random.permutation(cnt)
-    x_list = x_list[indices[:int(1000000)]]
-    s_list = [s_list[x] for x in indices[:int(1000000)]]
+    x_list = x_list[indices[:int(200000)]]
+    s_list = [s_list[x] for x in indices[:int(200000)]]
     print "truncation complete"
     gc.collect()
     print "garbage collection complete"
